a1239ae4cd733d82ae3ac52d73aa74b934c36aaf
Dimple3009 <114263330+Dimple3009@users.noreply.github.com>
2023-09-11 01:24:54 -0400

    Add files via upload

--- src/Components/Cuisines.tsx
+++ src/Components/Cuisines.tsx
@@ -1,5 +1,22 @@
 import React, { useState, useEffect } from 'react';
 
+const CUISINES_DATA = ['Mexican', 'Italian', 'Indian', 'South Indian', 'Pizza', 'Burger', 'Chinese', 'Mediterranean', 'English Breakfast', 'Vegan'];
+
+const cuisineToSearchTermMappingFood = [
+    'Indian',
+    'Mexican',
+    'Italian',
+    'South Indian',
+    'Chinese',
+    'Mediterranean',
+    'Vegan'
+]
+
+// mapping the array data to search term 
+const cuisineToSearchTermMappingExact = {
+    'English Breakfast': 'breakfast places',
+}
+
 function Cuisines(){
 
     const [cuisine, setCuisine] = useState('');
@@ -11,28 +28,46 @@ function Cuisines(){
     function handleClick() {
         return getCuisine();
     }
-    function handleCuisine() {
-        return 
-        
-    }
+    
 
 
     function getCuisine() {
-        let Cuisines = ['Mexican', 'Italian', 'Indian', 'South Indian', 'Pizza', 'Burger'];
-        let randomNum = Math.floor(Math.random() * Cuisines.length);
+        let randomNum = Math.floor(Math.random() * CUISINES_DATA.length);
         console.log(randomNum)
-        const RandomCuisine = Cuisines[randomNum];
+        const RandomCuisine = CUISINES_DATA[randomNum];
         setCuisine(RandomCuisine);
     }
     
+    function handleCuisine() {
+        // window.location.href = searchLink();
+        window.open(searchLink(), "_blank"); 
+       return ; 
+        
+    }
+    function searchLink() {
+        let searchTerm = '';
+
+        if(cuisineToSearchTermMappingFood.includes(cuisine)) {
+            searchTerm = cuisine.toLowerCase() + " food";
+        } else {
+            searchTerm = cuisineToSearchTermMappingExact[cuisine];
+        }
+        if(searchTerm === undefined) {
+            searchTerm = cuisine.toLowerCase();
+        }
+        let searchLink1 = "https://www.google.com/search?q="+ searchTerm +"+near+me&rlz=1C1CHBF_enIN943IN943&oq=google+pizza+near+me&aqs=chrome..69i57j0i22i30l9.7169j0j7&sourceid=chrome&ie=UTF-8";
+        return searchLink1;
+    }
+ 
+        
     return (
+        
         <div>
-            <h1>Cuisines</h1>
             <div id="box">
-            <div id="name"><a href="https://www.google.com/search?q=google+pizza+near+me&rlz=1C1CHBF_enIN943IN943&oq=google+pizza+near+me&aqs=chrome..69i57j0i22i30l9.7169j0j7&sourceid=chrome&ie=UTF-8">
+            <div id="name">
             <button  onClick={handleCuisine} ><h3>
                     {cuisine}
-                </h3></button></a></div>
+                </h3></button></div>
             <div><button id="new" onClick={handleClick}>
                     Something Else!
                 </button></div>
